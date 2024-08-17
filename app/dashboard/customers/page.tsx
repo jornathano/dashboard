2fe2ab4ff402a015dbd72c855f3133932b49826d
jornathano <jornathan@interia.pl>
2024-08-17 07:24:29 +0200

    utworzenie aplikacji

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,3 @@
+export default function Page() {
+    return <h1>Moi klienci</h1>;
+}
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,3 @@
+export default function Page() {
+    return <p className="text-center text-3xl text-sky-500">Dashboard Page</p>;
+  }
