ac3c4afa7490aae293cce6501b73ffbab68dff37
jornathano <jornathan@interia.pl>
2024-09-10 08:14:05 +0200

    Usuwanie faktur

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,6 +4,7 @@ import { z } from 'zod';
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import { Scheherazade_New } from 'next/font/google';
 
 const InvoiceSchema = z.object({
     id: z.string(),
@@ -34,4 +35,29 @@ export async function createInvoice(formData: FormData) {
 
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
+}
+
+const EditInvoice = InvoiceSchema.omit({id: true, date: true});
+
+export async function editInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = EditInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status')
+  });
+
+  const amountInCents = amount * 100;
+
+  await sql`
+    UPDATE invoices 
+    SET customer_id=${customerId}, amount=${amountInCents}, status=${status}
+    WHERE id=${id}
+  `;
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
+
+export async function deleteInvoice (id: string) {
+  await sql`DELETE FROM invoices WHERE id=${id}`;
+  revalidatePath('/dashboard/invoices');
 }
