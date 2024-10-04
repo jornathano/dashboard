a8b93c8e58426c72d29601775488f30076bc57ab
jornathano <jornathan@interia.pl>
2024-10-04 09:10:21 +0200

    Koniec

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { Metadata } from 'next';
 export const metadata: Metadata = {
   title: {
     template: '%s | Acme Dashboard',
-    default: 'Acme Dashboard',
+    default: 'Dashboard',
   },
   description: 'The official Next.js Course Dashboard, built with App Router.',
   metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
